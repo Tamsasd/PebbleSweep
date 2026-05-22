2fcd1116714e0f5b4e506229dcfe74e115f2862d
Horneczki Tamás <horneczki.tamas@mailbox.unideb.hu>
2026-05-22 17:43:14 +0200

    docs: add Javadoc for Position

--- src/main/java/pebblesweep/model/Position.java
+++ src/main/java/pebblesweep/model/Position.java
@@ -1,4 +1,10 @@
 package pebblesweep.model;
 
+/**
+ * Represents a coordinate on the board
+ *
+ * @param row the row index
+ * @param column the column index
+ */
 public record Position(int row, int column) {
 }
